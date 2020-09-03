c72c42f489da523d9b269dc60ba4058da7904748
pickdani <me@pickdani.com>
2020-09-02 22:48:26 -0400

    Merge remote-tracking branch 'origin/master' add solution

---
+++ Algorithms/easy/shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        for x in range(n):
+            ans.append(nums[x])
+            ans.append(nums[x+n])
+        return ans
